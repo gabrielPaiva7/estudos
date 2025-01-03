8eaad6a8174a151f3d75969c8a8985997f4d7631
gabrielPaiva7 <gabriel1352@protonmail.com>
2025-01-03 12:12:54 -0300

    Update basico3.js

--- BásicoJS3/basico3.js
+++ BásicoJS3/basico3.js
@@ -50,3 +50,59 @@ let personagem2 = {
   ],
 };
 console.log(personagem2.carros[0].modelo); //acessando o modelo do primeiro objeto do array
+
+console.log(`--------------------------------FUNCAO DENTRO DE OBJETO------------------------------------------`);
+
+let pessoa = {
+  nome: `Gabriel`,
+  sobrenome: `Paiva`,
+  idade: 90,
+  //A FUNCAO ARROW NAO TEM ACESSO AOS DADOS DO OBJETO
+  nomeCompleto: () => {
+    return 'blablabla';
+  },
+};
+console.log(`${pessoa.nome} ${pessoa.sobrenome}`);
+
+console.log(pessoa.nomeCompleto()); //chamando uma funcao que esta dentro de um objeto
+
+console.log('--------------------------------------------------------------------------------------------');
+
+let pessoa2 = {
+  nome: 'Gabriel',
+  sobrenome: 'Paiva',
+  idade: 90,
+  //ESSA FUNCAO EU TENHO ACESSO AOS DADOS DO OBJETO USANDO O THIS
+  nomeCompleto: function () {
+    return `${this.nome} ${this.sobrenome}`;
+  },
+};
+console.log(pessoa2.nomeCompleto()); //chamando uma funcao que esta dentro de um objeto
+
+console.log('--------------------------------------LOOP FOR------------------------------------------------------');
+
+// index = 10 é o contador inicial, ou seja o loop começa no numero 0, index<10 é ate onde esse contador vai, enquanto for menor que 10, vai se repetir, index++ vai incrementar o contador uma vez a cada repetição
+for (let index = 0; index <= 10; index++) {
+  console.log(index);
+  console.log('Gabriel'); //vai exibir gabriel 10 vezes
+}
+
+console.log('--------------------------------------LOOP EM ARRAY-----------------------------------------------------');
+
+let lista = ['preto', 'branco', 'azul', 'vermelho'];
+lista.push('verde');
+
+for (let n = 0; n < lista.length; n++) {
+  console.log(lista[n]);
+}
+
+console.log('--------------------------------------LOOP EM ARRAY DO JEITO CERTO--------------------------------------');
+
+let cores = ['azul', 'preto', 'vermelho', 'verde'];
+cores.push('rosa');
+
+for (let count in cores) {
+  console.log(cores[count]);
+}
+
+console.log('--------------------------------------LOOP EM ARRAY DO JEITO CERTO2------------------------------------');
